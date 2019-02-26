ac3760a14f1235af219248c7514556f3413d10a4
Will <wmfitt@Hotmail.com>
2019-02-25 21:16:30 -0700

    fixed delete button and clean up some things

--- main.js
+++ main.js
@@ -1,10 +1,3 @@
-
-//below stuff should be helpful for the project?
-//the row is creates font-size is smaller because the line performing that action has already been calledd. move line 19 below the append to fix this.
-// $(function(){
-//     $(".list").append("<div class='row'>Learn JQuery</div>");
-// });
-
 function addItem() {
     let myVal = $(".myInput").val();
     let listItems = $(".myList").children();
@@ -12,11 +5,10 @@ function addItem() {
 
         $(".myList").append(
             "<li  class='list-group-item' >"+ "<span contenteditable='true'>" + myVal + "</span>" +
-            "<button class='btn btn-dark float-right' type='button' onclick='deleteItem(this)'>Delete Item</button>" +
+            "<button class='btn btn-outline-secondary float-right' type='button' onclick='deleteItem(this)'>Delete Item</button>" +
             "</li>");
         $(".myInput").val("");
-        console.log(listItems);
-        $(".row:even").css("background-color", "#dadfea");
+
     }
     $(".myInput").focus();
 }
@@ -31,11 +23,11 @@ function checkKey(event){
 
 //the element parameter is arbitrary, could be anything as long as its the same in both spots
 function deleteItem(element) {
+    console.log("deleting");
     $(element).parent().fadeOut("medium", function(){
         $(element).parent().remove();
     });
-    //attempt to fix colors getting off, didnt work
-    $(".row:even").css("background-color", "#dadfea");
+
 
 
 }
