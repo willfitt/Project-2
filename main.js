4d2cf13b72e478a6bab6d986811a215a2b043831
Will <wmfitt@Hotmail.com>
2019-03-03 13:09:18 -0700

    added adding task functionality

--- main.js
+++ main.js
@@ -1,30 +1,37 @@
 
 let countId = 0;
-
+let taskId = 0;
 function count() {
     countId++;
     return countId;
 }
+
+function taskCount() {
+    taskId++;
+    return taskId;
+}
 function addItem() {
     let myVal = $(".myInput").val();
     let listItems = $(".myList").children();
-    let listArray = []
+    let listArray = [];
     if(myVal != ""){
         count();
+        taskCount();
         console.log(countId);
         $(".myList").append(
-            "<li class='list-group-item my-item' id='"+countId+"'>"+ "<div contenteditable='true'>" + myVal + "</div>" +
+            "<div class='itemContainer'>" +
+            "<li class='list-group-item my-item' id='count"+countId+"'>"+ "<div contenteditable='true'>" + myVal + "</div>" +
             "<div class='dropdown flex-nowrap'>" +
             "<button class='btn btn-secondary dropdown-toggle' type='button' id='dropdownMenu2' data-toggle='dropdown' aria-haspopup='true' aria-expanded=false'>" +
             "<i class='fas fa-cog'></i>" + " " + "</button>" +
             "<div class='dropdown-menu' aria-labelledby=dropdownMenu2'>" +
-            "<button class='btn btn-secondary dropdown-item' type='button' onclick='addTask(this)'>Add Task</button>" +
+            "<button class='btn btn-secondary dropdown-item' type='button' onclick='addTask(this, " + taskId + ")'>Add Task</button>" +
             "<button class='btn btn-warning dropdown-item' type='button' onclick='completeItem(event, " + countId + ")'>Complete</button>" +
             "<button class='btn btn-secondary dropdown-item' type='button' onclick='deleteItem(this, " + countId + ")'>Delete</button>" +
-            "</div>" + "</div>" +
-            "</li>");
+            "</div>" + "</div>" + "</li>" +
+            "<div class='myTaskList' id='task"+taskId+"'></div>" +
+            "</div>");
         $(".myInput").val("");
-
     }
     $(".myInput").focus();
 }
@@ -37,17 +44,23 @@ function checkKey(event){
     }
 }
 
-//the element parameter is arbitrary, could be anything as long as its the same in both spots
+function addTask(elemen, id) {
+    // let myTaskVal = $(".myTaskInput").val();
+    $("<div contenteditable='true'>...add task</div>").appendTo($("#task" + id));
+}
+
+
+
 function deleteItem(element, id) {
-    $("#" + id).fadeOut("medium", function(){
-        $("#" + id).remove();});
+    $("#count" + id).fadeOut("medium", function(){
+        $("#count" + id).remove();});
 }
 
 
 function completeItem(event, id) {
-    $("#" + id).fadeOut("medium", function(){
-        $(".completedList").append($("#" + id).innerHTML);
-        $("#" + id).remove();
+    $("#count" + id).fadeOut("medium", function(){
+        $(".completedList").append($("#count" + id).valueOf($(".my-item")));
+        $("#count" + id).remove();
     });
 }
 
@@ -59,11 +72,3 @@ function deleteAllItems() {
 
     }
 }
-
-
-
-function addTask(element) {
-
-}
-
-//FIX DROPDOWN BUTTON FUNCTIONS TO ACTUALLY WORK
